67f20aa838f36dc249a423f3ff7be09cb411fa51
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2021-03-23 14:06:37 +0100

    Create 'initialize' and remove_piece methods
    initialize - initialize with cords and piece with default value ' '
    remove_piece - set piece to ' '

--- lib/node.rb
+++ lib/node.rb
@@ -2,4 +2,15 @@
 
 # node used in board
 class Node
+  attr_reader :cords
+  attr_accessor :piece
+
+  def initialize(cords, piece = ' ')
+    @cords = cords
+    @piece = piece
+  end
+
+  def remove_piece
+    self.piece = ' '
+  end
 end
