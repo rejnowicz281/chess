7e7d452d49bdb94ded8af6cf73587aaea4b2fad0
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2021-03-24 12:41:31 +0100

    Created initialize. Created methods to show board in 2d view and to get individual nodes by their cords
    initialize - initialize size and nodes that take in the assign_nodes method
    assign_nodes - returns array with (size * size)x nodes that take in cords based on their position
    show_board - puts out nodes of the board in 2d view
    get_node - return node with given cords
    print_letter_cords - prints letter cords of the board

--- lib/board.rb
+++ lib/board.rb
@@ -4,4 +4,63 @@ require_relative 'node'
 
 # game board
 class Board
+  attr_reader :size
+  attr_accessor :nodes
+
+  def initialize(size = 8)
+    @size = size
+    @nodes = assign_nodes
+  end
+
+  def assign_nodes
+    nodes = []
+    letter_cord = 'a'
+    off_board_letter = (letter_cord.ord + size).chr
+    num_cord = size
+
+    until nodes.length == size * size
+      node = Node.new("#{letter_cord}#{num_cord}")
+      nodes << node
+      letter_cord = (letter_cord.ord + 1).chr
+
+      if letter_cord == off_board_letter
+        num_cord -= 1       # push in nodes with proper coordinates
+        letter_cord = 'a'
+      end
+    end
+
+    nodes
+  end
+
+  def show_board
+    print_letter_cords
+
+    nodes.each do |node|
+      case node.cords[0]
+      when 'a'
+        print " #{node.cords[1]} [ #{node.piece} ]"
+      when nodes.last.cords[0]
+        puts "[ #{node.piece} ] #{node.cords[1]}"
+      else
+        print "[ #{node.piece} ]"
+      end
+    end
+
+    print_letter_cords
+  end
+
+  def get_node(cords)
+    nodes.each { |node| return node if node.cords == cords }
+    nil
+  end
+
+  def print_letter_cords
+    letter_cords = ('a'..nodes.last.cords[0]).to_a
+    letter_cords.each { |letter| print letter == 'a' ?  "     #{letter}" : "    #{letter}" }
+    puts
+  end
 end
+
+b = Board.new
+
+b.show_board
