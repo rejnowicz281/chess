b9330b1c9b120e132b5a36a09266af7a7aeacc14
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2021-03-28 21:11:57 +0200

    Created chess_symbols module with chess symbols in unicode

---
+++ lib/pieces/chess_symbols.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# chess symbols in unicode
+module ChessSymbols
+  BLACK_PAWN = "\u2659"
+  WHITE_PAWN = "\u265f"
+
+  BLACK_ROOK = "\u2656"
+  WHITE_ROOK = "\u265c"
+
+  BLACK_BISHOP = "\u2657"
+  WHITE_BISHOP = "\u265d"
+
+  BLACK_KING = "\u2654"
+  WHITE_KING = "\u265a"
+
+  BLACK_KNIGHT = "\u2658"
+  WHITE_KNIGHT = "\u265e"
+
+  BLACK_QUEEN = "\u2655"
+  WHITE_QUEEN = "\u265b"
+end
