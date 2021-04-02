ab873f747e0c37da764f82ff2266e8091add8435
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2021-04-02 17:12:06 +0200

    Created 'movement' method that returns array with possible movement from that square

--- lib/square.rb
+++ lib/square.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
+Dir['pieces/*.rb'].each { |piece| require_relative piece }
+
 # square used in board
-
-require_relative 'pieces/piece'
-
 class Square
   attr_reader :cords, :file_cord, :rank_cord
   attr_accessor :piece
@@ -21,5 +20,19 @@ class Square
     else
       piece
     end
+  end
+
+  def movement
+    return if piece == ' '
+
+    movement = []
+    piece.movement_formula.each do |formula_cords|
+      movement_file_cord = (file_cord.ord + formula_cords[0]).chr
+      movement_rank_cord = (rank_cord + formula_cords[1])
+      movement << "#{movement_file_cord}#{movement_rank_cord}" unless movement_file_cord.ord < 'a'.ord ||
+                                                                      movement_file_cord.ord > 'h'.ord ||
+                                                                      movement_rank_cord < 1 || movement_rank_cord > 8
+    end
+    movement
   end
 end
