bc1360beae770472bbab351a45e15a71d48cfdd0
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2021-04-07 23:11:47 +0200

    Create module with methods to remove illegal pawn moves

---
+++ lib/pieces/pawn_behaviour.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# methods to give pawn correct behaviour depending on the neigbouring pieces
+module PawnBehaviour
+  def remove_illegal_pawn_moves(cords, legal_moves)
+    pawn_square = board.get_square(cords)
+
+    two_forward_cords = two_forward_from(cords)
+
+    forward_left_cords = forward_left_of(cords)
+    forward_left_square = board.get_square(forward_left_cords)
+
+    forward_right_cords = forward_right_of(cords)
+    forward_right_square = board.get_square(forward_right_cords)
+
+    legal_moves -= [two_forward_cords] unless pawn_square.piece.previous_move.nil?
+
+    legal_moves -= [forward_left_cords] if !forward_left_square.nil? &&
+                                           (forward_left_square.piece == ' ' ||
+                                           forward_left_square.piece.color == pawn_square.piece.color)
+
+    legal_moves -= [forward_right_cords] if !forward_right_square.nil? &&
+                                            (forward_right_square.piece == ' ' ||
+                                            forward_right_square.piece.color == pawn_square.piece.color)
+
+    legal_moves
+  end
+
+  def two_forward_from(cords)
+    rank = board.get_square(cords).piece.color == 'black' ? (cords[1].to_i - 2) : (cords[1].to_i + 2)
+    "#{cords[0]}#{rank}"
+  end
+
+  def forward_left_of(cords)
+    file = (cords[0].ord - 1).chr
+    rank = board.get_square(cords).piece.color == 'black' ? (cords[1].to_i - 1) : (cords[1].to_i + 1)
+    "#{file}#{rank}"
+  end
+
+  def forward_right_of(cords)
+    file = (cords[0].ord + 1).chr
+    rank = board.get_square(cords).piece.color == 'black' ? (cords[1].to_i - 1) : (cords[1].to_i + 1)
+    "#{file}#{rank}"
+  end
+end
