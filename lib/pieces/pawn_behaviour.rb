795df281ba2962b59f081f25509b17958cf3326b
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2021-04-10 21:17:42 +0200

    Add behaviour for one square forward moves

--- lib/pieces/pawn_behaviour.rb
+++ lib/pieces/pawn_behaviour.rb
@@ -5,6 +5,9 @@ module PawnBehaviour
   def remove_illegal_pawn_moves(cords, legal_moves)
     pawn_square = board.get_square(cords)
 
+    one_forward_cords = one_forward_from(cords)
+    one_forward_square = board.get_square(one_forward_cords)
+
     two_forward_cords = two_forward_from(cords)
 
     forward_left_cords = forward_left_of(cords)
@@ -12,6 +15,8 @@ module PawnBehaviour
 
     forward_right_cords = forward_right_of(cords)
     forward_right_square = board.get_square(forward_right_cords)
+
+    legal_moves -= [one_forward_cords] unless one_forward_square.piece == ' '
 
     legal_moves -= [two_forward_cords] unless pawn_square.piece.previous_move.nil?
 
@@ -24,6 +29,11 @@ module PawnBehaviour
                                             forward_right_square.piece.color == pawn_square.piece.color)
 
     legal_moves
+  end
+
+  def one_forward_from(cords)
+    rank = board.get_square(cords).piece.color == 'black' ? (cords[1].to_i - 1) : (cords[1].to_i + 1)
+    "#{cords[0]}#{rank}"
   end
 
   def two_forward_from(cords)
