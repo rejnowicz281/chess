d3d7d6c4d745c68416a2cc6bd8207dfda8886ded
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2021-04-24 19:52:50 +0200

    Add 'in_check' counter

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -4,6 +4,13 @@ require_relative 'piece'
 
 # king chess piece
 class King < Piece
+  attr_accessor :in_check_counter
+
+  def initialize(color)
+    @in_check_counter = 0
+    super
+  end
+
   def unicode
     color == 'black' ? BLACK_KING : WHITE_KING
   end
